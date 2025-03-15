0e0f36f580cc1bf02d92a2e8dadc797bb6fca7e6
Aryan Raj <aryan.raj@coupa.com>
2025-03-15 21:18:25 +0530

    Extracted line numbers from the diff
    Signed-off-by: Aryan Raj <aryan.raj@coupa.com>

--- cmd/pr-patch-coverage.go
+++ cmd/pr-patch-coverage.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 
 	"github.com/codeclimate/test-reporter/env"
@@ -85,6 +87,49 @@ func updatePrPatchOptions(args []string) error {
 	return nil
 }
 
+func getFileMappingFromDiff() error {
+	headBranchOrigin := "origin/" + prPatchOptions.headBranch
+	lineNumberRegex := regexp.MustCompile(`@@ -(?P<lineBefore>\d+),?(?P<countBefore>\d+)? \+(?P<lineAfter>\d+),?(?P<countAfter>\d+)? @@`)
+
+	for _, file := range prPatchOptions.prFiles {
+		diff, err := loadFromGit("diff", "-U0", headBranchOrigin, "HEAD", "--", file)
+		if err != nil {
+			return err
+		}
+
+		println("File: " + file)
+		for _, line := range strings.Split(diff, "\n") {
+			if strings.Contains(line, "@@") {
+				matches := lineNumberRegex.FindStringSubmatch(line)
+				if len(matches) >= 4 {
+					// Get names of the capturing groups
+					names := lineNumberRegex.SubexpNames()
+					println(fmt.Sprintf("Matches: %v, Names: %v", matches, names))
+					// Create a map of group names to their values
+					result := make(map[string]int)
+					for i, name := range names {
+						// names[i] is always an empty string from SubexpNames
+						if i != 0 && name != "" && i < len(matches) {
+							// Convert string to integer, default to 1 if empty
+							val := matches[i]
+							if val == "" {
+								val = "1"
+							}
+							num := 0
+							fmt.Sscanf(val, "%d", &num)
+							result[name] = num
+						}
+					}
+
+					println(fmt.Sprintf("Line before: %d, Count before: %d, Line after: %d, Count after: %d",
+						result["lineBefore"], result["countBefore"], result["lineAfter"], result["countAfter"]))
+				}
+			}
+		}
+	}
+	return nil
+}
+
 var prPatchCoverageCmd = &cobra.Command{
 	Use:   "pr-patch-coverage",
 	Short: "Generates patch coverage for PR. Needs to be run after format-coverage",
@@ -93,6 +138,11 @@ var prPatchCoverageCmd = &cobra.Command{
 			return err
 		}
 		println(prPatchOptions.String())
+
+		if err := getFileMappingFromDiff(); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
